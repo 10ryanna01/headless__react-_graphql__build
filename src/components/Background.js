7f78428a73d2fab5a363f32b040556b567536fc2
Ryann Al-lahiq <ryann.allahiq@gmail.com>
2021-10-12 19:07:06 +0100

    :art: updated loadtime for mobile

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,14 +2,11 @@ import React from "react";
 export default function PageBackground() {
   return (
     <>
-     
-        {/* 
-    <!-- <img class="inline__image"  style="background: grey" /> --> */}
-        <img
-          className="inline__image"
-          src="https://source.unsplash.com/2000x2800/?travel,festival,summer" alt="background images roates through image catagories of travel, festival and summer"
-        />
-    
+      <img
+        className="inline__image"
+        src="https://source.unsplash.com/1000x1800/?travel,boho,sunset&auto=format&fit=crop&w=1492&q=40"
+        alt="background images roates through image catagories of travel, festival and summer"
+      />
     </>
   );
 }
