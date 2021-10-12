2239affc4d2a43c2ac51e09a135ad78624452fb2
Ryann Al-lahiq <ryann.allahiq@gmail.com>
2021-10-12 22:44:56 +0100

    :zap: updated mobile view

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,8 +13,7 @@ const IndexPage = ({ data, pageContext, location }) => {
  
 
   return (
-    <> 
-
+    <>  
         <Helmet htmlAttributes={{ lang : "en" }}>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" /> 
